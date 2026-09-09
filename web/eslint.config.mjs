d7db0d113730855bea70e826574b9f9b6c973ad0
luffy0x <wangzhi_hao@163.com>
2026-09-09 14:51:55 +0800

    fix(test): isolate Next.js E2E output

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -9,7 +9,7 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
-    ".next-e2e/**",
+    ".next-e2e*/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
